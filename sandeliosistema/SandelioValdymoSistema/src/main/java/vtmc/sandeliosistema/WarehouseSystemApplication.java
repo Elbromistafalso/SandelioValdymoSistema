2f2afa8d54fe99f0c329eab1b518975cf92cb6c6
Paulius Savickis <paulsavickis@gmail.com>
2021-06-22 09:35:53 +0300

    added skeletons of Spring app and React app

---
+++ sandeliosistema/SandelioValdymoSistema/src/main/java/vtmc/sandeliosistema/WarehouseSystemApplication.java
@@ -0,0 +1,15 @@
+package vtmc.sandeliosistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import vtmc.sandeliosistema.WarehouseSystemApplication;
+
+@SpringBootApplication
+public class WarehouseSystemApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(WarehouseSystemApplication.class, args);
+    }
+
+}
